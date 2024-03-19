f6b7615f7d177c63e3441870087603f9780a016c
pagella42 <pagella42@gmail.com>
2024-03-19 18:47:36 -0300

    ui fix

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -24,11 +24,11 @@ export default function Page({ params }: { params: { id: string } }) {
         <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
         <BreadCrumbs id={id}/>
           <div className="mx-auto grid max-w-2xl grid-cols-1 grid-rows-1 items-start gap-x-8 gap-y-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {vehicles && vehicles[0] ? (
+            {vehicles && vehicles[0].unit_type ? (
               <VehicleCard isFirst vehicle={vehicles[0]} />
             ) : null}
             <MainDetails />
-            {vehicles && vehicles[1] ? (
+            {vehicles && vehicles[1].unit_type ? (
               <VehicleCard vehicle={vehicles[1]} />
             ) : null}
           </div>
